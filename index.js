156b92809f2683f227e1955436413565ee182cf8
JoseDarioTaborda <149601906+JoseDarioTaborda@users.noreply.github.com>
2023-11-27 21:11:03 -0300

    index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from 'express'
-import { readFile, writeFile  } from 'fs/promises' /*Codigo nuevo */
-import dotenv from 'dotenv'
+import { readFile, writeFile  } from 'fs/promises' 
 
+
+import productosRouter from './routes/productos.routes.js'
+import vendedoresRouter from './routes/vendedores.routes.js'
+import ventasRouter from './routes/ventas.routes.js'
 const app = express()
 
-const port = 3005
+const port = 3001
 
 app.use(express.json());
 
@@ -13,43 +16,6 @@ app.listen(port, () =>{
 })
 
 
-const file = await readFile('./JSON/vendedores.json', 'utf-8') /*Codigo nuevo */
-const userData = JSON.parse(file) /*Codigo nuevo */
-
-const objetos = [
-    {name: 'Auto', color: 'Rojo'},
-    {name:'Arbol', color:'Verde'},
-    {name: 'Rio', color:'Azul'},
-    {name: 'Casa', color: 'Amarillo'}
-]
-
-app.get('/', (req, res) => {
-    res.send('Hola mundo!')
-})
-
-app.get('/colorDe/:objeto',(req,res)=>{
-    const obj = req.params.objeto
-    const result = objetos.find(e => e.name === obj)
-
-    if(result){
-        res.status(200).json(result)
-    }else{
-        res.status(400).json(`${obj} no se encuentra`)
-    }
-    
-})
-
-app.post('/ColorDePost', (req,res)=>{
-    const obj = req.body.objeto
-    const result = objetos.find(e => e.name === obj)
-
-    if(result){
-        res.status(200).json(result)
-    }else{
-        res.status(404).json(`${obj} no se encuentra`)
-    }
-})
-
-app.get('/users/all', (req,res)=>{
-    res.status(200).json(userData)
-}) /*Codigo nuevo */
+app.use('/vendedores', vendedoresRouter)
+app.use('/productos', productosRouter)
+app.use('/ventas', ventasRouter)
